3357770ab0d4cf6e72f58d3476fb3bbdeec6cbfd
WGrot <wojtek.grot@onet.pl>
2024-03-10 13:02:42 +0100

    working inventory and stat UI panel

--- Projekt_Indywidualny/Assets/Scripts/Inventory System/UI/PassiveItemPanel.cs
+++ Projekt_Indywidualny/Assets/Scripts/Inventory System/UI/PassiveItemPanel.cs
@@ -7,101 +7,21 @@ using UnityEngine.UI;
 
 public class PassiveItemPanel : MonoBehaviour
 {
-    [SerializeField] GameObject itemList;
-    [SerializeField] GameObject itemSlotPrefab;
-    [SerializeField] GameObject passiveItemPanel;
-
     [SerializeField] private TextMeshProUGUI textBox;
     [SerializeField] private Image itemIcon;
 
-    private InputActions inputActions;
-
-    private RectTransform rt;
-    private bool isDirty = true;
-    private bool isPanelOpened = false;
     private PassiveItem displayedItem;
 
-    private void Start()
-    {
-        rt = itemList.GetComponent<RectTransform>();
-    }
-
-    private void Awake()
-    {
-        inputActions = new InputActions();
-        inputActions.UI.OpenInventory.performed += OpenCloseInventoryOnIPressed;
-    }
     private void OnEnable()
     {
-        Inventory.OnItemChangedCallback += SetDirty;
         InventorySlot.OnItemSelectedInInventoryCallback += ShowItemData;
-        inputActions.Enable();
-
     }
 
     private void OnDisable()
     {
-        Inventory.OnItemChangedCallback -= SetDirty;
         InventorySlot.OnItemSelectedInInventoryCallback -= ShowItemData;
-        inputActions.Disable();
     }
 
-    private void LoadPassiveItems()
-    {
-        for (int i = 0; i< itemList.transform.childCount; i++ )
-        {
-            Destroy(itemList.transform.GetChild(i).gameObject);
-        }
-
-        List<PassiveItem> passiveItemList = Inventory.Instance.GetPassiveItemList();
-        foreach (PassiveItem item in passiveItemList)
-        {
-            GameObject instance = Instantiate(itemSlotPrefab);
-            instance.transform.SetParent(itemList.transform, false);
-            instance.GetComponent<InventorySlot>().SetItem(item);
-
-            rt.sizeDelta = new Vector2(rt.rect.width, 35 * passiveItemList.Count);
-        }
-        isDirty = false;
-    }
-
-    private void SetDirty()
-    {
-        isDirty = true;
-    }
-
-
-    public void OpenCloseInventoryOnIPressed(InputAction.CallbackContext context)
-    {
-        if (isPanelOpened)
-        {
-            GameStateManager.Instance.ResumeGame();
-            passiveItemPanel.SetActive(false);
-            isPanelOpened = false;
-        }
-        else
-        {
-            
-            passiveItemPanel.SetActive(true);
-            if (isDirty)
-            {
-                LoadPassiveItems();
-            }
-            GameStateManager.Instance.PauseGame();
-            isPanelOpened=true;
-        }
-
-    }
-
-    public void CloseInventory()
-    {
-        if (isPanelOpened)
-        {
-            GameStateManager.Instance.ResumeGame();
-            passiveItemPanel.SetActive(false);
-            isPanelOpened = false;
-        }
-    }
 
     private void ShowItemData(Item item)
     {
@@ -117,7 +37,6 @@ public class PassiveItemPanel : MonoBehaviour
             return;
         }
         Inventory.Instance.RemovePassiveItem(displayedItem);
-        LoadPassiveItems();
         displayedItem = null;
         textBox.SetText("");
         itemIcon.sprite = null;
--- Projekt_Indywidualny/Assets/Scripts/Inventory System/UI/UIPanels.cs
+++ Projekt_Indywidualny/Assets/Scripts/Inventory System/UI/UIPanels.cs
@@ -1,13 +1,132 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UIPanels : MonoBehaviour
 {
     [SerializeField] private List<GameObject> uiPanels;
     private int activePanel;
-
     private bool isPanelOpened = false;
+    private InputActions inputActions;
 
+
+    #region passiveItemsPanelStuff
+    private bool isInventoryDirty = false;
+    private RectTransform rt;
+    [SerializeField] GameObject itemSlotPrefab;
+    [SerializeField] GameObject itemList;
+    #endregion
+
+    private void Start()
+    {
+        rt = itemList.GetComponent<RectTransform>();
+
+    }
+    private void Awake()
+    {
+        inputActions = new InputActions();
+        inputActions.UI.OpenInventory.performed += OpenCloseInventoryOnIPressed;
+    }
+
+    private void OnEnable()
+    {
+        Inventory.OnItemChangedCallback += SetInventoryDirty;
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        Inventory.OnItemChangedCallback -= SetInventoryDirty;
+        inputActions.Disable();
+    }
+
+    public void OpenCloseInventoryOnIPressed(InputAction.CallbackContext context)
+    {
+        if (isPanelOpened)
+        {
+            GameStateManager.Instance.ResumeGame();
+            uiPanels[activePanel].SetActive(false);
+            isPanelOpened = false;
+        }
+        else
+        {
+
+            uiPanels[0].SetActive(true);
+            if (isInventoryDirty)
+            {
+                LoadPassiveItems();
+            }
+            GameStateManager.Instance.PauseGame();
+            isPanelOpened = true;
+        }
+
+    }
+
+    public void ClosePanels()
+    {
+        if (isPanelOpened)
+        {
+            GameStateManager.Instance.ResumeGame();
+            uiPanels[activePanel].SetActive(false);
+            isPanelOpened = false;
+        }
+    }
+
+    public void SwitchToNextPanel()
+    {
+        uiPanels[activePanel].SetActive(false);
+        if (activePanel + 1 < uiPanels.Count)
+        {
+            uiPanels[activePanel + 1].SetActive(true);
+            activePanel += 1;
+        }
+        else
+        {
+            uiPanels[0].SetActive(true);
+            activePanel = 0;
+        }
+    }
+    public void SwitchToPreviousPanel()
+    {
+        uiPanels[activePanel].SetActive(false);
+        if (activePanel -1 >= 0)
+        {
+            uiPanels[activePanel -1].SetActive(true);
+            activePanel -= 1;
+        }
+        else
+        {
+            uiPanels[uiPanels.Count -1].SetActive(true);
+            activePanel = uiPanels.Count - 1;
+        }
+    }
+
+
+    #region passiveitemsPanel
+    private void SetInventoryDirty()
+    {
+        isInventoryDirty = true;
+    }
+
+    public void LoadPassiveItems()
+    {
+        for (int i = 0; i < itemList.transform.childCount; i++)
+        {
+            Destroy(itemList.transform.GetChild(i).gameObject);
+        }
+
+        List<PassiveItem> passiveItemList = Inventory.Instance.GetPassiveItemList();
+        foreach (PassiveItem item in passiveItemList)
+        {
+            GameObject instance = Instantiate(itemSlotPrefab);
+            instance.transform.SetParent(itemList.transform, false);
+            instance.GetComponent<InventorySlot>().SetItem(item);
+
+            rt.sizeDelta = new Vector2(rt.rect.width, 35 * passiveItemList.Count);
+        }
+        isInventoryDirty = false;
+    }
+    #endregion
 
 }
