38e9d2d7e73ce4a8d2de9e71f69d7edf4bfa0d75
WGrot <117313232+WGrot@users.noreply.github.com>
2024-09-18 12:48:56 +0200

    Added new enemy

--- Projekt_Indywidualny/Assets/Scripts/Enemies/AIscripts/DiggerAI.cs
+++ Projekt_Indywidualny/Assets/Scripts/Enemies/AIscripts/DiggerAI.cs
@@ -3,42 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class DiggerAI : EnemyHpBase
+public class DiggerAI : MonoBehaviour
 {
-    [SerializeField] private GameObject projectile;
+    //[SerializeField] private GameObject projectile;
     [SerializeField] private float damage;
     [SerializeField] private float waitTimeAfterShot;
     [SerializeField] private float burrowTime;
 
+    [SerializeField] private AudioClip burrowSound;
+    [SerializeField] private AudioClip unBurrowSound;
+    [SerializeField] private AudioClip attackSound;
 
+    private AudioSource audioSource;
+
+    private bool isPerformingAction = false;
+    private Animator animator;
     private GameObject player;
     [SerializeField] private Transform shootPoint;
-    public override void Start()
+    public void Start()
     {
-        base.Start();
         player = PlayerStatus.Instance.GetPlayerBody();
-        StartCoroutine(Teleport());
-        StartCoroutine(Attack());
+        animator = GetComponent<Animator>();
+        audioSource= GetComponent<AudioSource>();
 
     }
 
-    IEnumerator Attack()
+    private void Update()
     {
-        Vector3 bulletDirection = player.transform.position + new Vector3(0, 0.25f, 0) - shootPoint.position;
+        if (!isPerformingAction)
+        {
+            isPerformingAction = true;
+            StartCoroutine(PerformAction());
+        }
+    }
 
+    IEnumerator PerformAction()
+    {
+        //Burrowing
+        animator.SetBool("isBurrowing", true);
+        audioSource.clip = burrowSound;
+        audioSource.Play();
+        yield return new WaitForSeconds(burrowTime);
+        Vector3 destination = GetRandomPositionInRoom(transform.position, 8f);
+        transform.position = destination;
+        animator.SetBool("isBurrowing", false);
+        audioSource.clip = unBurrowSound;
+        audioSource.Play();
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
+
+        //Attacking
+        animator.SetBool("isAttacking", true);
+        audioSource.clip = attackSound;
+        audioSource.Play();
+        Vector3 bulletDirection = player.transform.position + new Vector3(0, 0.25f, 0) - shootPoint.position;
         EnemyProjectileObjectPool.Instance.ShootBullet(shootPoint.position, bulletDirection, 1, 10, 1);
         yield return null;
+        animator.SetBool("isAttacking", false);
+        yield return new WaitForSeconds(waitTimeAfterShot);
+        isPerformingAction = false;
     }
 
-    IEnumerator Teleport()
-    {
-
-            Vector3 Destination = GetRandomPositionInRoom(transform.position, 8f);
-            transform.position = Destination;
-            Debug.Log("teleported " + Destination.x + " " + Destination.y + " " + Destination.z);
-            yield return new WaitForSeconds(2f);
-
-    }
 
     private Vector3 GetRandomPositionInRoom(Vector3 startPos, float range)
     {
@@ -48,7 +73,7 @@ public class DiggerAI : EnemyHpBase
         NavMeshHit hit;
         if (NavMesh.SamplePosition(randomPoint, out hit, 2f, NavMesh.AllAreas)) 
         {
-            Debug.Log("chuj");
+            Debug.Log(hit.position.x + " " + hit.position.z);
             result = hit.position;
             return result;
         }
