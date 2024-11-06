c5fe7f450599792a61c956e790e84b8928f78314
WGrot <117313232+WGrot@users.noreply.github.com>
2024-11-06 22:06:31 +0100

    completed few secrets

--- Projekt_Indywidualny/Assets/Scripts/Inventory System/Pickups/UnlockComponent.cs
+++ Projekt_Indywidualny/Assets/Scripts/Inventory System/Pickups/UnlockComponent.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnlockWeapon : MonoBehaviour
+public class UnlockComponent : MonoBehaviour
 {
     public void UnlockTargetWeapon(int id)
     {
         SaveManager.Instance.UnlockWeapon(id);
+    }
+
+    public void UnlockTargetPassiveItem(int id)
+    {
+        SaveManager.Instance.UnlockItem(id);
     }
 }
