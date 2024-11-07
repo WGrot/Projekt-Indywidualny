c327230a8adaefbcc17e43e6b04126198d265c14
WGrot <117313232+WGrot@users.noreply.github.com>
2024-11-07 17:55:48 +0100

    Revert "some work on undiscovered map"
    This reverts commit 126fed897ca5cc2ac025bfa3849f796f78afd7c1.

--- Projekt_Indywidualny/Assets/Scripts/LevelGeneration/MiniMap/MiniMapGenerator.cs
+++ Projekt_Indywidualny/Assets/Scripts/LevelGeneration/MiniMap/MiniMapGenerator.cs
@@ -1,17 +1,11 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class MiniMapGenerator : MonoBehaviour
 {
     Texture2D miniMapSprite;
-    Texture2D undiscoveredTexture;
-    int size =256;
     public Texture2D MiniMapSprite { get => miniMapSprite; set => miniMapSprite = value; }
-
-    public Texture2D UndiscoveredTexture { get => undiscoveredTexture; set => undiscoveredTexture = value; }
 
     #region Singleton
     public static MiniMapGenerator Instance { get; private set; }
@@ -33,9 +27,8 @@ public class MiniMapGenerator : MonoBehaviour
 
     public void GenerateMiniMap()
     {
-        size = LevelGrid.Instance.GetSize();
+        int size = LevelGrid.Instance.GetSize();
         miniMapSprite = new Texture2D(size,size);
-        undiscoveredTexture = new Texture2D(size,size, TextureFormat.ARGB32, false);
 
         for(int i = 0; i < size; i++)
         {
@@ -61,47 +54,7 @@ public class MiniMapGenerator : MonoBehaviour
         }
         MiniMapSprite.filterMode= FilterMode.Point;
         MiniMapSprite.Apply();
-
-        undiscoveredTexture = Texture2D.blackTexture;
         Debug.Log("Generated Minimap");
-    }
-
-    public void UpdateUndiscoveredTexture(int playerPosX, int playerPosZ, int radius)
-    {
-        int radiusSquared = radius * radius;
-
-        // Ustalenie zakresu iteracji dla ograniczenia do prostok¹ta wokó³ okrêgu
-        int rowStart = Math.Max(0, playerPosX - radius);
-        int rowEnd = Math.Min(size - 1, playerPosX + radius);
-        int colStart = Math.Max(0, playerPosZ - radius);
-        int colEnd = Math.Min(size - 1, playerPosZ + radius);
-        Debug.Log(rowStart + " " + rowEnd + " " + colStart + " " + colEnd);
-        for (int row = rowStart; row <= rowEnd; row++)
-        {
-            for (int col = colStart; col <= colEnd; col++)
-            {
-                // Sprawdzamy, czy punkt (row, col) le¿y wewn¹trz okrêgu o œrodku (x, y)
-                int dx = row - playerPosX;
-                int dy = col - playerPosZ;
-
-                if (dx * dx + dy * dy <= radiusSquared)
-                {
-                    // Wywo³ujemy podan¹ funkcjê dla tego pola
-                    DiscoverPixel(row, col);
-                }
-            }
-        }
-
-        undiscoveredTexture.filterMode = FilterMode.Point;
-        undiscoveredTexture.Apply();
-    }
-
-
-
-    public void DiscoverPixel(int x, int y)
-    {
-        Color transparentColor = new Color(0, 0, 0, 0);
-        undiscoveredTexture.SetPixel(x, y, transparentColor);
     }
 
 }
